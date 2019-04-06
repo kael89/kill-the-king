4317f78be28160fee23ed2db848cfa44849633cf
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2019-04-06 14:24:55 +1100

    Add Confirmation Dialog component

---
+++ src/components/ConfirmationDialog.jsx
@@ -0,0 +1,32 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const ConfirmationDialog = ({ onConfirm, onCancel, open, text, title }) => (
+  <Dialog open={open}>
+    <DialogTitle>{title}</DialogTitle>
+    <DialogContent>
+      <DialogContentText>{text}</DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={onCancel}>Cancel</Button>
+      <Button onClick={onConfirm} color="primary">
+        OK
+      </Button>
+    </DialogActions>
+  </Dialog>
+);
+
+ConfirmationDialog.propTypes = {
+  onConfirm: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  title: PropTypes.string,
+  text: PropTypes.string.isRequired,
+};
+
+ConfirmationDialog.defaultProps = {
+  title: '',
+};
+
+export default ConfirmationDialog;
