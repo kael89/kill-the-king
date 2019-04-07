2e81f0d974d489fe869d6f327ea4f16a6f398374
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2019-04-07 10:55:49 +1000

    Rename Toggle Dialog action

--- src/store/modules/ui.js
+++ src/store/modules/ui.js
@@ -5,7 +5,7 @@ import themes from '../../themes';
 /* Actions */
 const CHANGE_THEME = `${APP_NAME}/ui/CHANGE_THEME`;
 const TOGGLE_BOARD_HINT = `${APP_NAME}/ui/TOGGLE_BOARD_HINT`;
-const TOGGLE_DIALOG_OPEN = `${APP_NAME}/ui/TOGGLE_DIALOG_OPEN`;
+const TOGGLE_DIALOG = `${APP_NAME}/ui/TOGGLE_DIALOG`;
 
 const defaultState = {
   dialogOpen: false,
@@ -20,7 +20,7 @@ export default function reducer(ui = defaultState, action) {
       return { ...ui, theme: action.theme };
     case TOGGLE_BOARD_HINT:
       return { ...ui, isHintVisible: action.visible };
-    case TOGGLE_DIALOG_OPEN:
+    case TOGGLE_DIALOG:
       return { ...ui, dialogOpen: action.open };
     default: {
       return ui;
@@ -46,10 +46,10 @@ export const hideBoardHint = () => ({
 
 export const openDialog = () => ({
   open: true,
-  type: TOGGLE_DIALOG_OPEN,
+  type: TOGGLE_DIALOG,
 });
 
 export const closeDialog = () => ({
   open: false,
-  type: TOGGLE_DIALOG_OPEN,
+  type: TOGGLE_DIALOG,
 });
