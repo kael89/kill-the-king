f21e1ea653aac9b564f8725474708d713441ea8a
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2019-04-14 16:28:38 +1000

    Fix issue with drag and drop

--- src/containers/DraggablePiece.jsx
+++ src/containers/DraggablePiece.jsx
@@ -6,15 +6,17 @@ import { addPiece, movePiece, removePiece } from '../store/modules/board';
 
 const mapDispatchToProps = dispatch => ({
   onDrop: (piece, dropData) => {
-    if (!dropData) {
-      // Piece was not dropped in a valid location
+    if (dropData === undefined) {
+      // Invalid drag and drop operation
       return null;
     }
 
-    if (!dropData.position) {
+    if (dropData === null) {
+      // Piece was dropped outside the board
       return dispatch(removePiece(piece.position));
     }
     if (!piece.position) {
+      // Piece was dragging from outside the board
       return dispatch(addPiece({ ...piece, position: dropData.position }));
     }
 
