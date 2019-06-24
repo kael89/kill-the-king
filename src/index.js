10ef0d5ec77e7ec4fb9278727fac26326dfa4788
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2019-06-25 00:00:07 +1000

    Supress linting errors

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+// eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
