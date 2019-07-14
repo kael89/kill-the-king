62c1dcb5d9fa168ae0855d2de8fc748f5b069178
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2019-07-14 11:20:53 +1000

    Define initial board in json

--- src/modules/board/index.js
+++ src/modules/board/index.js
@@ -1,2 +1,6 @@
-export { INITIAL_BOARD, addPiece, removePiece, movePiece } from './board';
+import initialBoard from './initialBoard';
+
+export const INITIAL_BOARD = initialBoard;
+
+export { addPiece, removePiece, movePiece } from './board';
 export { BoardJsonError, validateBoardJson } from './validateBoardJson';
