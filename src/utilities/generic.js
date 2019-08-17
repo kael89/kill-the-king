7601f579ae209037261602ec7a43712d046d4da9
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2019-08-18 01:20:46 +0800

    Add tests

--- src/utilities/generic.js
+++ src/utilities/generic.js
@@ -1,21 +1,15 @@
 import { withStyles, withTheme } from '@material-ui/core';
-import { snakeCase } from 'lodash';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 
 const JSON_INDENTATION = 2;
 
 /**
- * @param {string} text
- * @returns {string}
- */
-export const camelToKebabCase = text => snakeCase(text).replace('_', '-');
-
-/**
  * @param {any} value
  * @returns {boolean}
  */
-export const isHashObject = value => value && typeof value === 'object' && !Array.isArray(value);
+export const isHashObject = value =>
+  value !== null && typeof value === 'object' && !Array.isArray(value);
 
 /**
  * @param {any} input
@@ -31,9 +25,6 @@ const sortObjectKeys = object =>
     .sort()
     .reduce((result, key) => ({ ...result, [key]: object[key] }), {});
 
-/**
- * @param {string} error
- */
 export const showError = error => {
   // eslint-disable-next-line no-alert
   alert(error);
